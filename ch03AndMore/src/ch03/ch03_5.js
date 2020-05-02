3bbc8032fce2bcda33251cb91178905ffb9bef53
raccoonjeong <rana3782@gmail.com>
2020-05-03 00:30:49 +0900

    디렉토리 정리

--- ch03AndMore/src/ch03/ch03_5.js
+++ ch03AndMore/src/ch03/ch03_5.js
@@ -2,6 +2,7 @@
 exports.__esModule = true;
 // 타입 변환
 var person = { name: "J", age: 22 };
+// person.name; object에는 name이란 프로퍼티 없어서 에러남
 person.name;
 var obj = { name: 'ddd' };
 var name1 = obj.name;
